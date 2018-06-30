c5e73669e94e0f839c77fce718afe861e4e9c65c
Miggy Reyes <ynigoreyes@gmail.com>
2018-06-30 06:37:56 +0000

    now works off of docker hub's webhook

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -11,6 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const shell = require('shelljs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Successful\n');
-  shell.exec('/home/ubuntu/deploy-server/deploy.sh')
+router.post('/', function(req, res, next) {
+  let payload = req.body;
+  if (payload.repository.is_trusted && payload.push_data.tag === 'latest') {
+    shell.exec('/home/ubuntu/deploy-server/deploy.sh')
+    res.send('Deploying new version of website...\n');
+  }
+  res.send('Branch is not latest or an unauthorized post has been made')
 });
 
 module.exports = router;
